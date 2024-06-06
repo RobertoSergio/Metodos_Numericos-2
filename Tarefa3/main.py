cd4d857d35caf5852e0951774d19753d2be7554a
Roberto Sérgio Ribeiro de Meneses <108556774+RobertoSergio@users.noreply.github.com>
2024-06-05 22:16:28 -0300

    Mudanças na main
    Mudanças para que o usuário decida a abordagem e o grau que quer

--- Tarefa3/main.py
+++ Tarefa3/main.py
@@ -6,11 +6,11 @@ if __name__ == "__main__":
     print("---------------------------------------------")
     print("-------------------Tarefa 3------------------")
     print("---------------------------------------------\n")
-    print("A funcao usada sera: (sen(2x) + 4*(x)^2 + 3x)^2\n")
 
     programa = True
 
     while programa:
+        print("A funcao usada sera: (sen(2x) + 4*(x)^2 + 3x)^2\n")
         xi = int(input("Escreva o valor de xi:"))
         xf = int(input("Escreva o valor de xf:"))
         epilson = float(input("Escreva o valor do erro:"))
@@ -20,12 +20,40 @@ if __name__ == "__main__":
         print("2: Abordagem Fechada ")
         escolha = int(input("Digite o número da abordagem que você quer: "))
 
+        print("Escolha o grau desejado: ")
+        print("1: 1°grau ")
+        print("2: 2°grau ")
+        print("3: 3°grau ")
+        print("4: 4°grau ")
+        grau = int(input("Digite grau que você quer: "))
+        
         if escolha == 1:
-            print("Você escolheu a Abordagem Aberta")
-            integracao(xi, xf, epilson, escolha)
+            if grau == 1:
+                print("Você escolheu a Abordagem Aberta de 1°grau")
+                integracao(xi, xf, epilson, escolha, grau)
+            if grau == 2:
+                print("Você escolheu a Abordagem Aberta de 2°grau")
+                integracao(xi, xf, epilson, escolha, grau)
+            if grau == 3:
+                print("Você escolheu a Abordagem Aberta de 3°grau")
+                integracao(xi, xf, epilson, escolha, grau)
+            if grau == 4:
+                print("Você escolheu a Abordagem Aberta de 4°grau")
+                integracao(xi, xf, epilson, escolha, grau)
+
         if escolha == 2:
-            print("Você escolheu a Abordagem Fechada")
-            integracao(xi, xf, epilson, escolha)
+            if grau == 1:
+                print("Você escolheu a Abordagem Fechado de 1°grau")
+                integracao(xi, xf, epilson, escolha, grau)
+            if grau == 2:
+                print("Você escolheu a Abordagem Fechado de 2°grau")
+                integracao(xi, xf, epilson, escolha, grau)
+            if grau == 3:
+                print("Você escolheu a Abordagem Fechado de 3°grau")
+                integracao(xi, xf, epilson, escolha, grau)
+            if grau == 4:
+                print("Você escolheu a Abordagem Fechado de 4°grau")
+                integracao(xi, xf, epilson, escolha, grau)
 
         print("\n---------------------------------------------")
         print("1-Rodar o programa novamente ")
